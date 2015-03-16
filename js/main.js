9086bd313d04e432144c9b93991dcd03862fddea
rmaskara <rmaskara@emdeon.com>
2015-03-15 22:47:58 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,13 +18,25 @@ var app = {
     } else {
         alert(title ? (title + ": " + message) : message);
     }
-},
+}
+
+renderHomeView: function() {
+    var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+    $('body').html(html);
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+}
+
     initialize: function() {
-       this.store = new MemoryStore(function() {
-        self.showAlert('Store Initialized', 'Info');
+    var self = this;
+    this.store = new MemoryStore(function() {
+        self.renderHomeView();
     });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+}
 
 };
 
